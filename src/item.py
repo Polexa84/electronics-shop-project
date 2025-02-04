677e2d194710b598a0aea9244effaf8cf2904b5c
polexa84 <polexa84@gmail.com>
2025-02-04 03:52:53 +0300

    реализовано возможность сложения экземпляров класса Phone и Item (сложение по количеству товара в магазине) Реализована проверка, чтобы нельзя было сложить Phone или Item с экземплярами не Phone или Item классов.

--- src/item.py
+++ src/item.py
@@ -26,6 +26,11 @@ class Item:
     def __repr__(self):
         return f'{self.__class__.__name__}{self.__name, self.price, self.quantity}'
 
+    def __add__(self, other):
+        if not isinstance(other, Item):
+            raise ValueError('Складывать можно только объекты Item и дочерние от них.')
+        return self.quantity + other.quantity
+
 
     def calculate_total_price(self) -> float:
         """
